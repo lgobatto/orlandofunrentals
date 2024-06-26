28075859d69cec65cf213a6ba61f3f57bdc1a8dd
Leonardo Gobatto <lgobatto@studiovisual.com.br>
2024-06-26 12:13:28 -0300

    feat: Update rsync deployment configuration for wp-content directory
    The rsync deployment configuration for the wp-content directory has been updated to include additional switches and exclude the 'uploads' folder. This change ensures that the deployment process is more efficient and excludes unnecessary files.

--- src/wp-content/themes/orlandofunrentals/bud.config.ts
+++ src/wp-content/themes/orlandofunrentals/bud.config.ts
@@ -6,7 +6,10 @@ import type {Bud} from '@roots/bud';
 export default async (bud: Bud) => {
   bud
     .entry('app', ['@scripts/app.js', '@styles/app.scss'])
-    .assets(['images', 'fonts'])
+    .assets('images/**/*.{png,jpg,jpeg,svg,gif}', {
+      from: 'assets/images',
+      to: 'dist/images',
+    })
     .runtime(false)
     .splitChunks(false)
     .watch(['./views', 'app']);
